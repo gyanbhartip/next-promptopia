92e3a7a80616b5b9f6b8382c0a3956b4328c1904
Gyan Bharti <gyanbhartip@gmail.com>
2024-07-25 03:28:20 +0530

    fix: moved the useSearchParams() usage in a component wrapped in Suspense

---
+++ src/components/UpdatePrompt.tsx
@@ -0,0 +1,72 @@
+import Form from "_/components/Form";
+import type { Post, PostData } from "_/types";
+import { useRouter, useSearchParams } from "next/navigation";
+import { type FormEvent, useEffect, useState } from "react";
+
+const UpdateComponent = () => {
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const promptId = searchParams.get("id");
+
+  const [submitting, setSubmitting] = useState<boolean>(false);
+  const [post, setPost] = useState<PostData>({
+    prompt: "",
+    tag: "",
+  });
+
+  const updatePrompt = async (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setSubmitting(true);
+
+    if (!promptId) {
+      return alert("Prompt ID not found");
+    }
+
+    try {
+      const response = await fetch(`/api/prompt/${promptId}`, {
+        method: "PATCH",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          tag: post.tag,
+        }),
+      });
+      if (response.ok) {
+        setPost({ prompt: "", tag: "" });
+        router.push("/");
+      }
+    } catch (error) {
+      console.error("Error occurred while creating prompt", error);
+    } finally {
+      setSubmitting(false);
+    }
+  };
+
+  useEffect(() => {
+    const getPromptDetail = async () => {
+      const response = await fetch(`/api/prompt/${promptId}`);
+      const data = (await response.json()) as Post;
+
+      setPost({
+        prompt: data.prompt,
+        tag: data.tag,
+      });
+    };
+
+    if (promptId) {
+      getPromptDetail().catch((error) =>
+        console.error("Error fetching prompt", error),
+      );
+    }
+  }, [promptId]);
+  return (
+    <Form
+      type={"Edit"}
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={updatePrompt}
+    />
+  );
+};
+
+export default UpdateComponent;
